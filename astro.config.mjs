2ed7343abeaacb9c7253fdc0a57ad60d2098b5c6
Nathan <nathayoha@gmail.com>
2025-10-15 07:13:46 -0500

    fix formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code-tomato.github.io',
-  output: 'static'
+  output: 'static',
+  integrations: [tailwind()]
 });
